9fbb7a87376ce787492a6e3fef13a003067bbe41
Xander Kirsten <xanderkirsten1@gmail.com>
2022-10-31 18:32:49 +0200

    redo functions for better logic. add saving function for words not remembered

--- main.py
+++ main.py
@@ -1,71 +1,68 @@
 from tkinter import *
 import pandas
-from random import randint
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-data = pandas.read_csv('data/french_words.csv')
-data_list = data.to_dict(orient="records")
-
-
-# ----- Flipper ----- #
-def flipper():
-    global language
-    if language == 'French':
-        language = 'English'
-        lang_lbl.config(text=language, fg='white', bg='green')
-        word_lbl.config(text=dataset[language], fg='white', bg='green')
-        card.itemconfig(current_image, image=card_back)
-    else:
-        language = 'French'
-        lang_lbl.config(text=language, fg='black', bg='white')
-        word_lbl.config(text=dataset[language], fg='black', bg='white')
-        card.itemconfig(current_image, image=card_front)
-
-    window.after(3000, flipper)
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-# ----- Button functions -----#
-def wrong():
-    global dataset
-    random_pos = randint(0, len(data_list) - 1)
-    dataset = data_list[random_pos]
-    word_lbl.config(text=dataset[language])
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-def right():
-    global dataset
-    random_pos = randint(0, len(data_list) - 1)
-    dataset = data_list[random_pos]
-    word_lbl.config(text=dataset[language])
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
-# ----- UI-Setup ----- #
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
-window.title('Flash Card Learning')
+window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-card = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-card_front = PhotoImage(file='images/card_front.png')
-card_back = PhotoImage(file='images/card_back.png')
-current_image = card.create_image(400, 263, image=card_front)
-card.grid(column=1, row=1, columnspan=2)
+flip_timer = window.after(3000, func=flip_card)
 
-wrong_image = PhotoImage(file='images/wrong.png')
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=wrong)
-wrong_button.grid(column=1, row=2)
-right_image = PhotoImage(file='images/right.png')
-right_button = Button(image=right_image, highlightthickness=0, command=right)
-right_button.grid(column=2, row=2)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-language = 'French'
-lang_lbl = Label(text=language, font=('Ariel', 40, 'italic'), bg='white')
-lang_lbl.place(x=400, y=150, anchor='center')
-random_pos = randint(0, len(data_list) - 1)
-dataset = data_list[random_pos]
-word_lbl = Label(text=dataset[language], font=('Ariel', 60, 'bold'), bg='white')
-word_lbl.place(x=400, y=253, anchor='center')
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-flipper()
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
